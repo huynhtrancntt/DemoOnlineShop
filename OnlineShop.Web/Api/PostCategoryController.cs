f25bdf98a28019fd29d6c140550e1fe7a5354903
HT-DELL\HT <huynhtrancntt@gmail.com>
2017-03-10 23:54:23 +0700

    auto mapper

--- OnlineShop.Web/Api/PostCategoryController.cs
+++ OnlineShop.Web/Api/PostCategoryController.cs
@@ -1,6 +1,10 @@
-﻿using OnlineShop.Model.Models;
+﻿using AutoMapper;
+using OnlineShop.Model.Models;
 using OnlineShop.Service;
 using OnlineShop.Web.Infrastructure.Core;
+using OnlineShop.Web.Infrastructure.Extensions;
+using OnlineShop.Web.Models;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -10,7 +14,7 @@ namespace OnlineShop.Web.Api
     [RoutePrefix("api/postcategory")]
     public class PostCategoryController : ApiControllerBase
     {
-        private IPostCategoryService _postCategoryService;
+        IPostCategoryService _postCategoryService;
 
         public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService) :
             base(errorService)
@@ -25,13 +29,16 @@ namespace OnlineShop.Web.Api
             {
                 var listCategory = _postCategoryService.GetAll();
 
-                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listCategory);
+                var listPostCategoryVm = Mapper.Map<List<PostCategoryViewModel>>(listCategory);
+
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listPostCategoryVm);
 
                 return response;
             });
         }
 
-        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
+        [Route("add")]
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -42,16 +49,21 @@ namespace OnlineShop.Web.Api
                 }
                 else
                 {
-                    var category = _postCategoryService.Add(postCategory);
+                    PostCategory newPostCategory = new PostCategory();
+                    newPostCategory.UpdatePostCategory(postCategoryVm);
+
+                    var category = _postCategoryService.Add(newPostCategory);
                     _postCategoryService.Save();
 
                     response = request.CreateResponse(HttpStatusCode.Created, category);
+
                 }
                 return response;
             });
         }
 
-        public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
+        [Route("update")]
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategoryViewModel postCategoryVm)
         {
             return CreateHttpResponse(request, () =>
             {
@@ -62,10 +74,13 @@ namespace OnlineShop.Web.Api
                 }
                 else
                 {
-                    _postCategoryService.Update(postCategory);
+                    var postCategoryDb = _postCategoryService.GetById(postCategoryVm.ID);
+                    postCategoryDb.UpdatePostCategory(postCategoryVm);
+                    _postCategoryService.Update(postCategoryDb);
                     _postCategoryService.Save();
 
                     response = request.CreateResponse(HttpStatusCode.OK);
+
                 }
                 return response;
             });
@@ -86,6 +101,7 @@ namespace OnlineShop.Web.Api
                     _postCategoryService.Save();
 
                     response = request.CreateResponse(HttpStatusCode.OK);
+
                 }
                 return response;
             });
---
+++ OnlineShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using OnlineShop.Model.Models;
+using OnlineShop.Web.Models;
+
+namespace OnlineShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>().MaxDepth(2);
+                cfg.CreateMap<Post, PostViewModel>().MaxDepth(2);
+                cfg.CreateMap<Tag, TagViewModel>().MaxDepth(2);
+                cfg.CreateMap<ProductTag, PostTagViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Ebook, EbookViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Page, PageViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseCategory, CourseCategoryViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Course, CourseViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppGroup, AppGroupViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppRole, AppRoleViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppUser, AppUserViewModel>().MaxDepth(2);
+                //cfg.CreateMap<AppUserGroup, AppUserGroupViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Trainer, TrainerViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseVideo, CourseVideoViewModel>().MaxDepth(2);
+                //cfg.CreateMap<TechLine, TechLineViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseUser, CourseUserViewModel>().MaxDepth(2);
+                //cfg.CreateMap<CourseReview, CourseReviewViewModel>().MaxDepth(2);
+                //cfg.CreateMap<PaymentMethod, PaymentMethodViewModel>().MaxDepth(2);
+                //cfg.CreateMap<TransactionLog, TransactionLogViewModel>().MaxDepth(2);
+                //cfg.CreateMap<VideoComment, VideoCommentViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Notification, NotificationViewModel>().MaxDepth(2);
+                //cfg.CreateMap<Footer, FooterViewModel>().MaxDepth(2);
+
+            });
+        }
+    }
+}
